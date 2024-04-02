8e926813edd293720b509a405e79a92bd7ff913f
Jackie <1121@generative.xyz>
2024-04-03 02:18:35 +0700

    update(ui): register page

--- src/modules/Register/index.tsx
+++ src/modules/Register/index.tsx
@@ -1,7 +1,59 @@
+import Image from "next/image";
 import React from "react";
 
+import Button from "@/components/Button";
+import FieldContainer from "@/components/FieldContainer";
+import { TextInput } from "@/components/Input";
+import { PasswordInput } from "@/components/Input/TextInput";
+import Label from "@/components/Label";
+import Typography from "@/components/Typography";
+import { BRAND_NAME } from "@/constants/metadata";
+import { AUTH_ROUTE_PATH } from "@/constants/routePath";
+
 const RegisterModule = () => {
-  return <div>RegisterModule</div>;
+  return (
+    <div className="w-full h-full flex">
+      <div className="h-full w-1/2 hidden lg:block">
+        <Image src="/images/register-background.jpg" alt="Login background" width={1920} height={1080} />
+      </div>
+
+      <div className="h-full flex-1 flex justify-center items-center">
+        <div className="lg:w-2/5">
+          <Typography tag="h1" fontSize="3xl" textAlign="center">
+            Register to {BRAND_NAME}
+          </Typography>
+
+          <form>
+            <FieldContainer>
+              <Label htmlFor="email">Email</Label>
+              <TextInput id="email" type="email" placeholder="example@xyz.com" sizeElement="sm" />
+            </FieldContainer>
+
+            <FieldContainer>
+              <Label htmlFor="password">Password</Label>
+              <PasswordInput id="password" placeholder="********" sizeElement="sm" />
+            </FieldContainer>
+
+            <FieldContainer className="mb-[2.4rem]">
+              <Label htmlFor="confirm_password">Confirm Password</Label>
+              <PasswordInput id="confirm_password" placeholder="********" sizeElement="sm" />
+            </FieldContainer>
+
+            <Button className="w-full" rounded="square" type="submit">
+              Register
+            </Button>
+          </form>
+
+          <FieldContainer className="mt-[1.6rem] flex justify-center">
+            <Typography fontSize="sm">Already have an account?&nbsp;</Typography>
+            <Button as="a" href={AUTH_ROUTE_PATH.LOGIN} variant="text" fontSize="sm">
+              Login now
+            </Button>
+          </FieldContainer>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterModule;
